47e527cd69bf04d40c6f1e212abdb666187026d3
Rahul <rahuldeo2047@gmail.com>
2018-10-31 16:49:59 +0530

    Initial working commit

---
+++ catkin_workspace/src/hello_world/src/ros_getting_started_node.cpp
@@ -0,0 +1,36 @@
+#include "ros/ros.h"
+
+/**
+ * This tutorial demonstrates simple sending of messages over the ROS system.
+ */
+int main(int argc, char **argv)
+{
+  /**
+   * The ros::init() function needs to see argc and argv so that it can perform
+   * any ROS arguments and name remapping that were provided at the command line.
+   * For programmatic remappings you can use a different version of init() which takes
+   * remappings directly, but for most command-line programs, passing argc and argv is
+   * the easiest way to do it.  The third argument to init() is the name of the node.
+   *
+   * You must call one of the versions of ros::init() before using any other
+   * part of the ROS system.
+   */
+  ros::init(argc, argv, "printer");
+
+  /**
+   * NodeHandle is the main access point to communications with the ROS system.
+   * The first NodeHandle constructed will fully initialize this node, and the last
+   * NodeHandle destructed will close down the node.
+   */
+  ros::NodeHandle n;
+
+  ROS_INFO("STARTED MY FIRST ROS NODE named as %s", ros::this_node::getName().c_str());
+
+  while (ros::ok())
+  {
+
+  }
+
+
+  return 0;
+}
